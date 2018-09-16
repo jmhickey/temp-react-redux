c8b119d43009cfe80c8108ca67a44fd4138cc983
jason <jason.m.hickey@gmail.com>
2018-09-16 14:34:57 -0700

    Update for react-redux course

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,3 +1,4 @@
+import webpack from 'webpack';
 import path from 'path';
 
 export default {
@@ -5,19 +6,30 @@ export default {
   devtool: 'inline-source-map',
   noInfo: false,
   entry: [
+    'webpack-hot-middleware/client?reload=true', //note that it reloads the page if hot module reloading fails.
     path.resolve(__dirname, 'src/index')
   ],
   target: 'web',
   output: {
-    path: path.resolve(__dirname, 'src'),
+    path: path.resolve(__dirname, '/dist'), // Note: Physical files are only output by the production build task `npm run build`
     publicPath: '/',
     filename: 'bundle.js'
   },
-  plugins: [],
+  devServer: {
+    contentBase: './src'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loaders: ['babel']},
-      {test: /\.css$/, loaders: ['style','css']}
+      {test: /\.css$/, loaders: ['style','css']},
+      {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: "file"},
+      {test: /\.(woff|woff2)$/, loader: "url?prefix=font/&limit=5000"},
+      {test: /\.tff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream"},
+      {test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=image/svg+xml"}
     ]
   }
-}
+};
