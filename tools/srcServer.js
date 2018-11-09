6c0c7d1cce02338ef6ae76eed8bfcfcb6d5866a2
jason <jason@wostmann.com>
2018-11-08 18:17:47 -0800

    Switch out open library due to vulnerability; upgrade mocha

--- tools/srcServer.js
+++ tools/srcServer.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import path from 'path';
-import open from 'open';
+import opn from 'opn';
 import webpack from 'webpack';
 import config from '../webpack.config.dev';
 
@@ -25,6 +25,6 @@ app.listen(port, function(err) {
   if (err) {
     console.log(err);
   } else {
-    open(`http://localhost:${port}`);
+    opn(`http://localhost:${port}`);
   }
 });
