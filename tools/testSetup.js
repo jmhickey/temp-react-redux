639f01950630fedaec9a38ef3794efdd27f90bb3
jason <jason@wostmann.com>
2018-11-13 17:30:42 -0800

    Remove ES2015 syntax

--- tools/testSetup.js
+++ tools/testSetup.js
@@ -42,12 +42,12 @@ const jsdom = new JSDOM('<!doctype html><html><body></body></html>');
 const { window } = jsdom;
 
 function copyProps(src, target) {
-  const props = Object.getOwnPropertyNames(src)
-    .filter(prop => typeof target[prop] === 'undefined')
-    .reduce((result, prop) => ({
-      ...result,
-      [prop]: Object.getOwnPropertyDescriptor(src, prop),
-    }), {});
+  const props = {};
+  Object.getOwnPropertyNames(src).forEach((prop) => {
+    if (typeof target[prop] === 'undefined') {
+      props[prop] = Object.getOwnPropertyDescriptor(src, prop);
+    }
+  });
   Object.defineProperties(target, props);
 }
 
