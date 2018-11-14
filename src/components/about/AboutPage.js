63eeccd9d81761a4389d9e5fb585104efe3b1814
jason <jason@wostmann.com>
2018-11-13 17:30:50 -0800

    Add description about mock API

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -5,7 +5,9 @@ class AboutPage extends React.Component {
     return (
       <div>
         <h1>About</h1>
-        <p>This application uses React, Redux, React Router and a variety of other helpful libraries.</p>
+        <p>This application uses React, Redux, React Router and a variety of other libraries.</p>
+        <p>To switch to using the standalone mock REST endpoint (with generated data), append the URL
+           query parameter &quot;useMockApi=true&quot; to the URL. (Note: this option is only available in development builds.)</p>
       </div>
     );
   }
